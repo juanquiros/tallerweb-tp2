9e22171db7d6b14873577b42cd2caf54f34dbae6
juanquiross <juanquiross@gmail.com>
2022-04-22 10:04:07 -0300

    eje 1 b

--- js/eje_1_b.js
+++ js/eje_1_b.js
@@ -1,21 +1,24 @@
 var concatenado = "";
-function agregar(string){
-    if(string.value){
-        concatenado = concatenado + string.value + ';';
+
+
+
+function agregar(string){    
+    if(string.value){        
+        this.concatenado += string.value + ';';
         string.value = '';
-        console.log(concatenado);
-    }
-    
+        console.log(this.concatenado);
+    }    
 }
 
 function finalizar(){    
-    if(concatenado!=""){
-        var split = concatenado.split(';');
-        var str_html='<ol>'
+    if(this.concatenado!=""){
+        var split = this.concatenado.split(';');
+        var str_html = '<ol id="lista">';
         for (var i=0; i < split.length-1; i++) {
             str_html+= '<li>'+split[i]+'</li>';
-         }         
-         str_html+='</ol>'
+         }     
+         str_html+='</ol>';
          document.getElementById('lista').outerHTML = str_html;
+         this.concatenado='';
     }
 }
